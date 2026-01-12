d29560bfa90d31154b88219c197783dd7b73b00e
Prototypestore <dikeledireeks@outlook.com>
2026-01-12 21:19:18 +0200

    Update carousel.js

--- carousel.js
+++ carousel.js
@@ -1,16 +1,32 @@
 let images = [];
 let currentIndex = 0;
+let preloaded = [];
 
 fetch("carousel.json")
   .then(res => res.json())
   .then(data => {
     images = data.carousel;
+    preloadImages();
     createCarousel();
     renderImage();
   });
 
+/* -----------------------------
+   PRELOAD IMAGES (KEY FIX)
+------------------------------ */
+function preloadImages() {
+  images.forEach(item => {
+    const img = new Image();
+    img.src = item.image;
+    preloaded.push(img);
+  });
+}
+
+/* -----------------------------
+   CREATE CAROUSEL
+------------------------------ */
 function createCarousel() {
-  const main = document.getElementById("content");
+  const main = document.getElementById("carousel");
 
   main.innerHTML = `
     <div class="carousel">
@@ -28,11 +44,23 @@ function createCarousel() {
   document.querySelector(".next").addEventListener("click", next);
 }
 
+/* -----------------------------
+   RENDER IMAGE WITH FADE
+------------------------------ */
 function renderImage() {
   const img = document.getElementById("carousel-image");
-  img.src = images[currentIndex].image;
+
+  img.style.opacity = "0";
+
+  setTimeout(() => {
+    img.src = images[currentIndex].image;
+    img.style.opacity = "1";
+  }, 180);
 }
 
+/* -----------------------------
+   CONTROLS
+------------------------------ */
 function next() {
   currentIndex = (currentIndex + 1) % images.length;
   renderImage();
